2aac4ae1bf0b443535ab0c27a8b3aa78b14605fe
lonebaggie <simon.walker@mac.com>
2018-06-03 09:17:25 +0100

    Added notify

---
+++ appdaemon/apps/LeakWarning.py
@@ -0,0 +1,13 @@
+# smtp warning of Water Leak
+import appdaemon.plugins.hass.hassapi as hass
+class Leakwarning(hass.Hass):
+    def initialize(self):
+        self.listen_state(self.leakwarning,"binary_sensor.water_leak_sensor_158d0001c3472b",new="on")
+        self.listen_state(self.leakwarning,"binary_sensor.water_leak_sensor_158d0001c347c9",new="on")
+    def leakwarning (self, entity, attribute, old, new, kwargs):
+        mess= "Leak Detected by " + self.friendly_name(entity)
+        self.log(mess)
+        self.notify(mess, title = "Leak Warning", name = "notify.gmail_alert")
+        self.call_service("persistent_notification/create", message = mess, title = "Leak Warning")
+        
+        
