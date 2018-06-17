106a3dac7ba097d8e5789d75c5e1d6caea5897dc
lonebaggie <simon.walker@mac.com>
2018-06-17 11:13:13 +0100

    Light sync in UI

--- appdaemon/apps/LRSwitch.py
+++ appdaemon/apps/LRSwitch.py
@@ -4,7 +4,15 @@ class LRswitch(hass.Hass):
     def initialize(self):
         self.listen_event(self.lrswitch, "click", entity_id = "binary_sensor.wall_switch_158d0001830c7c", click_type = "single")
     def lrswitch(self,event_name, data, kwargs):
-        self.toggle("light.all_living_room_lights")
+        if self.get_state("light.all_living_room_lights") == "off" :
+            self.turn_on("input_boolean.relaxed")
+        else :
+            self.turn_off("light.all_living_room_lights")
+
+            
+            
+            
+      
 
         
             
--- appdaemon/apps/OSAutolight.py
+++ appdaemon/apps/OSAutolight.py
@@ -7,7 +7,7 @@ class OSautolight(hass.Hass):
     def initialize(self):
         self.listen_state(self.osautolight,"binary_sensor.motion_sensor_158d0001e43e18")
     def osautolight (self, entity, attribute, old, new, kwargs):
-        if sun_down() :
+        if self.sun_down() :
             self.turn_on("switch.wall_switch_158d0001614701")
             self.run_in(self.light_off, 120)
     def light_off(self, kwargs):
