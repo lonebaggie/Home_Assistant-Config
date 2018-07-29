4096fabd1e5142fb951d04cc4a793cf4f214599d
lonebaggie <simon.walker@mac.com>
2018-07-29 07:59:59 +0100

    fix input.text

--- appdaemon/apps/SpeakAlexa.py
+++ appdaemon/apps/SpeakAlexa.py
@@ -1,9 +1,9 @@
 import appdaemon.plugins.hass.hassapi as hass
 class Speakalexa(hass.Hass):
     def initialize(self):
-        self.listen_state(self.speakalexa,"input_text.alexa")
+        self.listen_state(self.speakalexa,"input_text.tts")
     def speakalexa (self, entity, attribute, old, new, kwargs):
-        talk = self.get_state("input_text.alexa")
+        talk = self.get_state("input_text.tts")
         if talk != "" :
             self.log("TTS Called")
             self.call_service("media_player/alexa_tts",entity_id="media_player.lr_dot",message=talk)
