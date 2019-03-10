15a07035af8fd2f21fa5bd181dfd4d9a86ffc998
lonebaggie <simon.walker@mac.com>
2019-03-10 09:04:51 +0000

    github fix

--- appdaemon/apps/OSAutolight.py
+++ appdaemon/apps/OSAutolight.py
@@ -5,15 +5,11 @@ import appdaemon.plugins.hass.hassapi as hass
 import datetime
 class OSautolight(hass.Hass):
     def initialize(self):
-<<<<<<< HEAD
-=======
-        time = datetime.time(22, 59, 59)
->>>>>>> f4b966840133ddbf4c76aeb6ec8ee6e1df7554e9
         self.run_at_sunset(self.osautolight)
         self.run_at_sunrise(self.osautolight_off)
     def osautolight (self, kwargs):
         if self.get_state("sensor.allhome") != "true" :
-            self.log("Auto Outside light off")
+            self.log("Auto Outside light on")
             self.turn_on("light.outside_light")
     def osautolight_off (self, kwargs):
         self.log("Auto Outside light off")
